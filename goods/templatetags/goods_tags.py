2afb6e78304ac3e353ef5505e20d5315381a6731
Bohdan Kukuruza <bohdan.kukuruza2@mail.dcu.ie>
2025-07-04 00:39:29 +0100

    connected postgreasql

--- goods/templatetags/goods_tags.py
+++ goods/templatetags/goods_tags.py
@@ -9,7 +9,7 @@ register = template.Library()
 
 @register.simple_tag()
 def tag_categories():
-    return Categories.objects.all()
+    return Categories.objects.all().order_by("id")
 
 @register.simple_tag(takes_context=True)
 def change_params(context, **kwargs):
